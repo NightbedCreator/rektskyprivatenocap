efdeb1589a7f78d21aea3806a3c7a07359c19a7b
NightbedCreator <105342825+NightbedCreator@users.noreply.github.com>
2022-05-20 18:04:17 +0700

    Update bedwars.lua

--- scripts/bedwars.lua
+++ scripts/bedwars.lua
@@ -312,7 +312,7 @@ end
 
 local isclone = false
 
---[[function kickKillauraRemote()
+function kickKillauraRemote()
     if (not isclone) then
         local mouse = game.Players.LocalPlayer:GetMouse()
         for i,v in pairs(game.Players:GetChildren()) do
@@ -342,7 +342,7 @@ local isclone = false
             end
         end 
     end
-end--]]
+end
 
 function getclosebed()
     for i,v in pairs(game:GetService("Workspace").Map.Blocks:GetChildren()) do
@@ -564,6 +564,7 @@ Tabs["Combat"]:CreateToggle({
         end
     end
 })
+end
 
 local TPAURAFUNIv2
 local tpaurafunihaha
@@ -620,6 +621,7 @@ tpaurafunihaha = Tabs["Combat"]:CreateToggle({
         end
     end
 })
+end
 
 local ShieldRemote = getremote(debug.getconstants(debug.getprotos(getmetatable(KnitClient.Controllers.ShieldController).raiseShield)[1]))
 local connectionkaurablock
@@ -645,7 +647,7 @@ Tabs["Combat"]:CreateToggle({
         end
     end
 })
---]]
+end
 
 do
     local velohorizontal = {["Value"] = 0}
@@ -1393,7 +1395,58 @@ Tabs["Render"]:CreateToggle({
     ["Keybind"] = nil,
     ["Callback"] = function(v)
         if v and entity.isAlive then
-            Cape(game.Players.LocalPlayer.Character, getasset("rektsky/cape.png"))
+local player = game:GetService("Players")
+local lplr = player.LocalPlayer
+if lplr.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
+      if lplr.Character:FindFirstChild("Torso") then
+        torso = lplr.Character.Torso
+      else
+        torso = lplr.Character.UpperTorso
+      end
+      local CapeP = Instance.new("Part", torso.Parent)
+      CapeP.Name = "Cape"
+      CapeP.Anchored = false
+      CapeP.CanCollide = false
+      CapeP.TopSurface = 0
+      CapeP.BottomSurface = 0
+      CapeP.Color = Color3.fromRGB(255,255,255)
+      CapeP.FormFactor = "Custom"
+      CapeP.Size = Vector3.new(0.2,0.2,0.2)
+      local decal = Instance.new("Decal", CapeP)
+      decal.Texture = "http://www.roblox.com/asset/?id=9600268824"
+      decal.Face = "Back"
+      local msh = Instance.new("BlockMesh", CapeP)
+      msh.Scale = Vector3.new(9,17.5,0.5)
+      local motor = Instance.new("Motor", CapeP)
+      motor.Part0 = CapeP
+      motor.Part1 = torso
+      motor.MaxVelocity = 0.01
+      motor.C0 = CFrame.new(0,1.75,0) * CFrame.Angles(0,math.rad(90),0)
+      motor.C1 = CFrame.new(0,1,0.45) * CFrame.Angles(0,math.rad(90),0)
+      local wave = false
+      repeat wait(1/44)
+        decal.Transparency = torso.Transparency
+        local ang = 0.1
+        local oldmag = torso.Velocity.magnitude
+        local mv = 0.002
+        if wave then
+          ang = ang + ((torso.Velocity.magnitude/10) * 0.05) + 0.05
+          wave = false
+        else
+          wave = true
+        end
+        ang = ang + math.min(torso.Velocity.magnitude/11, 0.5)
+        motor.MaxVelocity = math.min((torso.Velocity.magnitude/111), 0.04) + mv
+        motor.DesiredAngle = -ang
+        if motor.CurrentAngle < -0.2 and motor.DesiredAngle > -0.2 then
+          motor.MaxVelocity = 0.04
+        end
+        repeat wait() until motor.CurrentAngle == motor.DesiredAngle or math.abs(torso.Velocity.magnitude - oldmag) >= (torso.Velocity.magnitude/10) + 1
+        if torso.Velocity.magnitude < 0.1 then
+          wait(0.1)
+        end
+      until not CapeP or CapeP.Parent ~= torso.Parent
+    end
         else
             Cape(game.Players.LocalPlayer.Character, nil)
         end
